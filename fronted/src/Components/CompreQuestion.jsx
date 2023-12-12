4b265fe14f6add200b5758965445e983a68b0be7
Uma Sahni <monkeytown64@gmail.com>
2023-12-13 01:11:25 +0530

    Made css clean

--- fronted/src/Components/CompreQuestion.jsx
+++ fronted/src/Components/CompreQuestion.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import axios from "axios"
-
+import axios from "axios";
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { FaPlus } from "react-icons/fa";
+import { FaGripLines, FaTrash  } from 'react-icons/fa';
 const CompreQuestion = () => {
   const [questions, setQuestions] = useState([
     { id: 1, text: '', options: ['', '', '', ''], correct: 0 },
   ]);
-
-  const [format, setFormat] = useState(null)
 
   const handleAddQuestion = () => {
     setQuestions([...questions, { id: questions.length + 1, text: '', options: ['', '', '', ''], correct: 0 }]);
@@ -36,79 +36,110 @@ const CompreQuestion = () => {
     );
   };
 
-  useEffect(() => {
-    // This will be automatically called whenever questions state changes
-    const backendFormat = questions.map((question) => ({
-      userId: 'radha1',
-      question: question.text,
-      options: question.options.map((option, index) => ({
-        text: option,
-        isCorrect: index === question.correct,
-      })),
-    }));
-    setFormat(backendFormat)
-    console.log(backendFormat);
-  }, [questions]); // Specify questions as a dependency for useEffect
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
 
+    const updatedOptions = Array.from(questions[0].options);
+    const [reorderedOption] = updatedOptions.splice(result.source.index, 1);
+    updatedOptions.splice(result.destination.index, 0, reorderedOption);
+
+    setQuestions((prevQuestions) => [
+      { ...prevQuestions[0], options: updatedOptions },
+    ]);
+  };
 
   const handleSubmit = () => {
-    axios.post(`http://localhost:8080/mcq/add`, format)
-    .then((res)=>{
-      console.log(res)
+    // Send the questions data to the backend using axios.post
+    axios.post(`http://localhost:8080/mcq/add`, {
+      userId: 'radha1',
+      questionType: 'MCQ',
+      question: questions[0].text,
+      points: 0,  // You may adjust this based on your requirement
+      options: questions[0].options.map((option, index) => ({
+        text: option,
+        isCorrect: index === questions[0].correct,
+      })),
     })
-    .catch((err)=>{
-      console.log(err)
-    })
-  }
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div className="container mx-auto mt-8 p-4">
-      <h2 className="text-lg font-bold mb-4">Form Editor</h2>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="container mx-auto mt-8 p-4">
+        <h2 className="text-lg font-bold mb-4">Form Editor</h2>
 
-      {questions.map((question) => (
-        <div key={question.id} className="mb-4 flex">
-          <div className="flex-1 pr-4">
-            <label className="block mb-2">Question:</label>
-            <textarea
-              value={question.text}
-              onChange={(e) => handleQuestionChange(question.id, 'text', e.target.value)}
-              className="w-1/2 p-2 border"
-            />
+        {questions.map((question) => (
+          <Droppable key={question.id} droppableId="options" type="OPTIONS">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="mb-4 flex"
+              >
+                <div className="flex-1 pr-4">
+                  <label className="block mb-2">Question:</label>
+                  <textarea
+                    value={question.text}
+                    onChange={(e) => handleQuestionChange(question.id, 'text', e.target.value)}
+                    className="w-1/2 p-2 border"
+                  />
 
-            <label className="block mt-4 mb-2">Options:</label>
-            {question.options.map((option, index) => (
-              <div key={index} className="mb-2 flex items-center">
-                <input
-                  type="radio"
-                  name={`correct-answer-${question.id}`}
-                  checked={question.correct === index}
-                  onChange={() => handleOptionChange(question.id, index, option)}
-                  className="mr-2"
-                />
-                <input
-                  type="text"
-                  value={option}
-                  onChange={(e) => handleOptionChange(question.id, index, e.target.value)}
-                  placeholder={`Option ${index + 1}`}
-                  className="w-1/2 p-2 border"
-                />
+                  <label className="block mt-4 mb-2">Options:</label>
+                  {question.options.map((option, index) => (
+                    <Draggable key={index} draggableId={`option-${index}`} index={index}>
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className="mb-2 flex items-center"
+                        >
+                          <FaGripLines className='mr-2' />
+                          <input
+                            type="radio"
+                            name={`correct-answer-${question.id}`}
+                            checked={question.correct === index}
+                            onChange={() => handleOptionChange(question.id, index, option)}
+                            className="mr-2"
+                          />
+                          <input
+                            type="text"
+                            value={option}
+                            onChange={(e) => handleOptionChange(question.id, index, e.target.value)}
+                            placeholder={`Option ${index + 1}`}
+                            className="w-1/2 p-2 border"
+                          />
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+                <div className="flex-shrink-0 flex flex-col items-end">
+                  <button onClick={handleAddQuestion} className="mb-4 bg-blue-500 text-white p-2">
+                    <FaPlus />
+                  </button>
+
+                  <button onClick={() => handleRemoveQuestion(question.id)} className="mb-4 bg-red-500 text-white p-2">
+                   <FaTrash />
+                  </button>
+                  
+                </div>
               </div>
-            ))}
-          </div>
-          <div className="flex-shrink-0 flex flex-col items-end">
-            <button onClick={() => handleRemoveQuestion(question.id)} className="mb-4 bg-red-500 text-white p-2">
-              Remove Question
-            </button>
-            <button onClick={handleAddQuestion} className="bg-blue-500 text-white p-2">
-              Add Question
-            </button>
-          </div>
-        </div>
-      ))}
+            )}
+          </Droppable>
+        ))}
 
-      <button onClick={handleSubmit} >Save</button>
-
-    </div>
+        <button className='bg-green-600 pr-3 pl-3' onClick={handleSubmit}>Save</button>
+      </div>
+    </DragDropContext>
   );
 };
 
